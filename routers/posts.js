886d40cb90d1be5a0d9062592b7feabab3b780ad
MicheleTodeschini <micheletodeschini01@gmail.com>
2025-11-26 12:13:25 +0100

    add const for calling the controller in posts

--- routers/posts.js
+++ routers/posts.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const router = express.Router()
+const articoliController = require('../controllers/articoliController')
+
 
 // Per richiamarli su Postman, usare http://localhost:3000/api/posts/
 
